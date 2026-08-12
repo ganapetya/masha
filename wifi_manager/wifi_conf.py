1febe47424a240c8198a68ae2e645a0b1add5423
peter <peter.kovgan@gmail.com>
2026-08-12 21:58:21 +0300

    Stop tracking wifi_conf.py; ignore secrets, keep example

--- wifi_manager/wifi_conf.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-#coding:utf8
-
-WIFI_MODE = 2                               #wifi的工作模式， 1为AP模式， 2为STA模式
-#WIFI_AP_SSID = 'WN-Robot'                  #AP模式下的SSID。字符和数字构成,需要以 WN- 开头，否则app功能无法使用
-WIFI_AP_PASSWORD = '12345678'        #AP模式下的WIFI密码,字符和数字构成
-WIFI_STA_SSID = 'TP-Link-Bot-Net-High'            #STA模式下的SSID
-WIFI_STA_PASSWORD = 'Tm16967$15P'    #STA模式下的WIFI密码 
